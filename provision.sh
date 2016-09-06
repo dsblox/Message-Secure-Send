3802c3008acad45134db50899b5a0a11234b6842
David Block <dblock@dblock-laptop.datto.lan>
2016-09-06 15:11:21 -0400

    added provision script

--- provision.sh
+++ provision.sh
@@ -1,4 +1,6 @@
-echo "Provisioning MSS..."
+echo "MSS: Provisioning MSS..."
+echo "MSS: Pulling dsblox/mss image from Docker Hub..."
 docker pull dsblox/mss
+echo "MSS: Running dsblox/mss image as server daemon..."
 docker run -d -p 4000:4000 --name mss dsblox/mss
-echo "...done!"
+echo "MSS: Message Secure Send Server running on port 4000." 
